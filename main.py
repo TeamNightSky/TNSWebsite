3c638b774e2b0e90a5ba3264bfbea2720ea781b5
Wizard Hat <Nlarsen23.student@gmail.com>
2020-10-19 21:25:39 +0000

    Replaced some remote resources with local resources

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html', home_href='/', about_href='/', join_href='/', projects_href='/', emails='zceboys@gmail.com', emails_href='/')
+
+@app.route('/github')
+def github():
+    return redirect('https://github.com/TeamNightSky')
 
 
 if __name__ == '__main__':
