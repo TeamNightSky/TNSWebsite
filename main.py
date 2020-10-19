d051a933957ebd9a91d34fc9b214a31d65154901
Wizard Hat <Nlarsen23.student@gmail.com>
2020-10-19 21:03:41 +0000

    Initial Changes

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html', home_href='/', about_href='/', join_href='/', projects_href='/', emails='zceboys@gmail.com', emails_href='/')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
